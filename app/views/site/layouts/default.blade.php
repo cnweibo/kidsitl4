9a3b32622aff2f36e06b6db379887f44177a5f48
root <root@ef504420-0a33-4974-859f-2d8f832d02a9>
2014-05-16 17:02:15 +0000

    none

--- app/views/site/layouts/default.blade.php
+++ app/views/site/layouts/default.blade.php
@@ -6,7 +6,7 @@
 		<meta charset="utf-8" />
 		<title>
 			@section('title')
-			IT fffffffff
+			IT宝贝网-IT范儿来学习,IT从娃娃抓起，IT成为新世纪必备的技能
 			@show
 		</title>
 		<meta name="keywords" content="your, awesome, keywords, here" />
