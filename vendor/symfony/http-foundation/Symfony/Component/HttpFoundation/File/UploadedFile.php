4613f7d2f5f31cbd0525829c81f146aa21bfe9cb
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2014-06-05 16:09:37 +0000

    upload file ok

--- vendor/symfony/http-foundation/Symfony/Component/HttpFoundation/File/UploadedFile.php
+++ vendor/symfony/http-foundation/Symfony/Component/HttpFoundation/File/UploadedFile.php
@@ -236,7 +236,6 @@ class UploadedFile extends File
             }
 
             $target = $this->getTargetFile($directory, $name);
-dd($target);
             if (!@move_uploaded_file($this->getPathname(), $target)) {
                 $error = error_get_last();
                 throw new FileException(sprintf('Could not move the file "%s" to "%s" (%s)', $this->getPathname(), $target, strip_tags($error['message'])));
