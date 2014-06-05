b7d1dcc024756e7a71d547df8164963c4a71c113
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2014-06-05 16:12:39 +0000

    uploaded done view displayed when upload done

---
+++ trunk/app/views/sandstudy/uploaddone.blade.php
@@ -0,0 +1,7 @@
+@extends('sandstudy.layouts.master')
+@section('content')
+<div class="alert alert-success">
+	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+	<strong>上传成功</strong> 文件上传成功！
+</div>
+@stop
