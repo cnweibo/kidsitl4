481b2143a21f208f299a88f2fa1f8b598667f29e
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2014-06-15 13:37:19 +0000

    introduce ajax search bishun

--- app/views/site/bishun/bishun.blade.php
+++ app/views/site/bishun/bishun.blade.php
@@ -1,5 +1,13 @@
 @extends('site.layouts.default')
-
+<div class="container">
+{{Form::open(['method' =>'POST','url'=>'http://kidsit.cn/bishun'])}}
+	<div class="form-group pull-right">
+		<label for="">输入汉字查询笔顺</label>
+		<input type="text" class="bishunsearch" id="bishunsearch" placeholder="输入要查询的字">
+		<button type="submit" class="btn btn-primary">查询</button>
+	</div>
+{{Form::close()}};	
+</div>
 {{-- Content for bishun flash --}}
 @section('contentbishun')
 	@include('site.bishun.bishunpartial')
