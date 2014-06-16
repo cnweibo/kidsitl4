ecf2d13205bc4b0bd895ea6605d49628ce5b4d48
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2014-06-16 15:50:36 +0000

    如果笔顺搜索为空时，返回index页面

--- trunk/app/controllers/BishunController.php
+++ trunk/app/controllers/BishunController.php
@@ -36,14 +36,17 @@ class BishunController extends \BaseController {
 	public function postSearch()
 	{
 		$bishunsearch = Input::get('bishunsearch');
-		$bishun = Bishun::where('hanzi','=', $bishunsearch)->first();
-		// dd(get_class($bishuns));
-		// dd($bishuns);
-		return (View::make('site.bishun.bishunSearchPartial',compact('bishun')));
-		// populate the html markup which will be displayed in ajax page
-		//show the searched bishun items
-		$bishuns = Bishun::all();
-		return View::make('site.bishun.bishun', compact('bishuns'));   
+		if ($bishunsearch){
+			$bishun = Bishun::where('hanzi','=', $bishunsearch)->first();
+			// dd(get_class($bishuns));
+			// dd($bishuns);
+			return (View::make('site.bishun.bishunSearchPartial',compact('bishun')));
+		}else{
+			// populate the html markup which will be displayed in ajax page
+			//show the searched bishun items
+			$bishuns = Bishun::all();
+			return View::make('site.bishun.bishun', compact('bishuns'));   
+		}
 	}
 	/**
 	 * Show the form for creating a new resource.
