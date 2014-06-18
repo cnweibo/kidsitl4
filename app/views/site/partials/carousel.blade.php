2edabb1f20d8ffa64288f8f95eacdb3226990a0b
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2014-06-18 15:25:57 +0000

    change homepage

--- app/views/site/partials/carousel.blade.php
+++ app/views/site/partials/carousel.blade.php
@@ -31,6 +31,19 @@
 		<span class="glyphicon glyphicon-chevron-left"></span>
 	</a>
 	<a data-slide="next" href="#carousel_id" class="right carousel-control">
+		<ul>
+		    <li class="list-unstyled text-left text-warning">孩子学习无方，效率低下？</li>		    
+		    <li class="list-unstyled text-left text-info">沉迷游戏</li>
+		    <li class="list-unstyled text-left text-warning">不能自拔？</li>
+		</ul>
+		<hr>
+		<p>IT宝贝网</p>
+		<ul>
+			<li class="text-left text-success">践行现代IT技术辅助学生学习</li>
+			<li class="text-left text-info">推动IT成为学生必备生存技能</li>
+			<li class="text-left text-success">家长从此不再谈网色变</li>
+			<li class="text-left text-info">让IT成为学生的style：IT范儿</li>
+		</ul>
 		<span class="glyphicon glyphicon-chevron-right"></span>
 	</a>
 </div>
