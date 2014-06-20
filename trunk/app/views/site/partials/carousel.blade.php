b884cb74e3843725687f1955de427a8faf004fd0
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2014-06-20 13:36:50 +0000

    首页carasoule更改，很美观

--- trunk/app/views/site/partials/carousel.blade.php
+++ trunk/app/views/site/partials/carousel.blade.php
@@ -8,22 +8,27 @@
     <div class="item active"> 
 		<img src="{{asset('assets/img/internet.jpg')}}" class="carouselimg" alt="互联网那点事">
 		<div class="carousel-caption">
-		<h4>让网络成为一种工具</h4>
-		<p>网络已经成为新生代的生活常态，有的家长视为洪水猛兽，IT宝贝因势利导，让网络成为一种服务学生的工具！</p>
+		<h4>让IT成为宝贝的必备技能</h4>
+		<p>“计算机从娃娃抓起。” IT宝贝网倡导并践行适合中国学生的信息教育</p>
+		<p>让IT技能成为中国学生的生存必备技能，成就学生的IT范儿</p>
+	
 	</div>
 	</div>
     <div class="item">
 		<img src="{{asset('assets/img/ITPowered.jpg')}}" class="carouselimg" alt="">
 		<div class="carousel-caption">
-		<h4>Second Thumbnail label</h4>
-		<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+		<h4>IT技术辅助日常学习</h4>
+		<p>现代IT技术发展带来学习的革命，网络也让学生获取信息更加简洁便利</p>
+		<p>IT宝贝网践行现代化的IT技术辅助学生的日常学习，</p>
+		<p>让学习不仅高效而且有趣</p>
 		</div>
 	</div>
     <div class="item">
 		<img src="{{asset('assets/img/littletreeSmall.jpg')}}" class="carouselimg" alt="">
 		<div class="carousel-caption">
-		<h4>Third Thumbnail label</h4>
-		<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+		<h4>我们的小树苗我们应该如何爱护和培养？</h4>
+		<p>家长从此不再谈网色变</p>
+		<p>让IT成为学生的style：IT范儿</p>
 		</div>
     </div>
 	</div>
@@ -31,19 +36,7 @@
 		<span class="glyphicon glyphicon-chevron-left"></span>
 	</a>
 	<a data-slide="next" href="#carousel_id" class="right carousel-control">
-		<ul>
-		    <li class="list-unstyled text-left text-warning">孩子学习无方，效率低下？</li>		    
-		    <li class="list-unstyled text-left text-info">沉迷游戏</li>
-		    <li class="list-unstyled text-left text-warning">不能自拔？</li>
-		</ul>
-		<hr>
-		<p>IT宝贝网</p>
-		<ul>
-			<li class="text-left text-success">践行现代IT技术辅助学生学习</li>
-			<li class="text-left text-info">推动IT成为学生必备生存技能</li>
-			<li class="text-left text-success">家长从此不再谈网色变</li>
-			<li class="text-left text-info">让IT成为学生的style：IT范儿</li>
-		</ul>
+		
 		<span class="glyphicon glyphicon-chevron-right"></span>
 	</a>
 </div>
