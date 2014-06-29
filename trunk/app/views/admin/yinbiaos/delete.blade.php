ec522ff6b8f13b37949523d782599da01ff2dbb7
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2014-06-29 13:47:47 +0000

    实现了后台音标删除功能

--- trunk/app/views/admin/yinbiaos/delete.blade.php
+++ trunk/app/views/admin/yinbiaos/delete.blade.php
@@ -8,19 +8,19 @@
             <li class="active"><a href="#tab-general" data-toggle="tab">General</a></li>
         </ul>
     <!-- ./ tabs -->
-    {{-- Delete bishun Form --}}
-    <form id="deleteForm" class="form-horizontal" method="post" action="@if (isset($bishun)){{ URL::to('admin/bishuns/' . $bishun . '/delete') }}@endif" autocomplete="off">
+    {{-- Delete yinbiao Form --}}
+    <form id="deleteForm" class="form-horizontal" method="post" action="@if (isset($yinbiao)){{URL::route('postyinbiaodelete',$yinbiao,['class'=>'yinbiaoatag']) }}@endif" autocomplete="off">
         
         <!-- CSRF Token -->
         <input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
-        <input type="hidden" name="hanzi" value="{{ $bishun }}" />
+        <input type="hidden" name="id" value="{{ $yinbiao }}" />
         <!-- <input type="hidden" name="_method" value="DELETE" /> -->
         <!-- ./ csrf token -->
 
         <!-- Form Actions -->
         <div class="form-group">
             <div class="controls">
-                删除 {{$bishun}} 的笔顺信息？                
+                删除 {{Yinbiao::find($yinbiao)->name}} 的音标信息？                
                 <element class="btn-cancel close_popup"><button class="btn btn-primary">取消</button></element>
                 <button type="submit" class="btn btn-danger">删除</button>
             </div>
