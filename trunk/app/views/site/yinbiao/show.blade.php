6200a7350ffcb84871e40a5671254dd09428eadf
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2014-06-29 08:43:40 +0000

    增加音标声音，使用HTML5 audio tag，jquery控制。todo:desktop使用flash方式。

--- trunk/app/views/site/yinbiao/show.blade.php
+++ trunk/app/views/site/yinbiao/show.blade.php
@@ -4,8 +4,12 @@
 		<ul class="list-unstyled">
 		<li>
 		<div style="font-family:Lucida Sans Unicode,Arial Unicode MS;letter-spacing: 5px;font-size:20px">
-			 <h1>{{link_to_route('yinbiao.show',$yinbiao->name,$yinbiao->id,['class'=>'yinbiaoatag'])}}</h1>
-			 <h2>所属类别：{{link_to_route('yinbiaocategory.show',$yinbiao->yinbiaocategory->ybcategory,$yinbiao->yinbiaocategory->id,['class'=>'yinbiaocatatag'])}} </h2>
+		<div>
+		<h1 style="display:inline;margin-right:20px">{{link_to_route('yinbiao.show',$yinbiao->name,$yinbiao->id,['class'=>'yinbiaoatag'])}}</h1>
+		<span id="yinbiao_1"><i class="clickable glyphicon glyphicon-volume-up"></i>点击发音</span>
+		<audio id="yinbiaoplayer"></audio>
+		</div>
+		<h2>所属类别：{{link_to_route('yinbiaocategory.show',$yinbiao->yinbiaocategory->ybcategory,$yinbiao->yinbiaocategory->id,['class'=>'yinbiaocatatag'])}} </h2>
 		</div>
 		</li>
 		<li>{{link_to_route('yinbiao.index','浏览全部音标')}}</li>
