709c51bf3c09cf58e13fbcc96008c22c6dbf9880
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2014-07-12 14:01:32 +0000

    1.bugfix for duplicate of words fayinguize 2.delte the pivot entry when the word deleted

--- app/models/Yinbiao.php
+++ app/models/Yinbiao.php
@@ -6,9 +6,11 @@ class Yinbiao extends \Eloquent {
 	public function yinbiaocategory(){
 		return $this->belongsTo('Yinbiaocategory');
 	}
-	public function relatedwords(){
-		return $this->belongsToMany('Relatedword');
-	}
+	// remove the relation between yinbiao and word.This relation works
+	// by help of fayinguize table
+	// public function relatedwords(){
+	// 	return $this->belongsToMany('Relatedword');
+	// }
 	public function fayinguizes(){
 		return $this->hasMany('Fayinguize');
 	}	
