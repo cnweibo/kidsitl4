fa5c93d8dc2a546282f5a7d654baf5b19573c9c3
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2014-07-30 07:24:42 +0000

    $http.get retrieve the added word for angular JS app

--- trunk/public/bootstrap/js/custom.js
+++ trunk/public/bootstrap/js/custom.js
@@ -74,20 +74,25 @@ $('#bishunsearchform #inputBishunsearch').bind('keyup',function(e){
 });
 // angular js guest add word controller
 function guestaddwordController($scope,$log,$http){
-	$scope.wordsadded = [
-		{wordtext: 'hello',createdby: 'zhangsan'},
-		{wordtext: 'go',createdby: 'lisi'},
-	];
-	// $http.get('/guestaddedword').success(function(guestaddedwords))
-	// {
-	// 	$scope.wordsadded = guestaddedwords;
-	// };
+	// $scope.wordsadded = [
+	// 	{wordtext: 'hello',createdby: 'zhangsan'},
+	// 	{wordtext: 'go',createdby: 'lisi'},
+	// ];
+	$http.get('/guestaddedword').success(function(guestaddedwords)
+	{
+		$scope.wordsadded = guestaddedwords;
+	});
 	$scope.addword = function(){
+		var wordadded = {
+			wordtext: $scope.newwordadded,
+			createdby: 'zhangsan'
+		};
+		$http.post('/guestaddedword',wordadded);
 		$scope.wordsadded.push({
 			wordtext: $scope.newwordadded,
 			createdby:'wangwu'
 		});
-		$log.info($scope.wordsadded);
+		// $log.info($scope.wordsadded);
 		$scope.newwordadded = '';
 	};
 
