667d5b5b3a8cba8153250443caffdbb405d6ef14
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2014-07-31 18:29:49 +0000

    我来加词功能增加客户端regex验证，只能是字母组成的单词可以提交

--- trunk/app/views/getjs/guestaddjs.blade.php
+++ trunk/app/views/getjs/guestaddjs.blade.php
@@ -6,6 +6,7 @@ guestaddwordapplication.controller('guestaddwordController_<?php echo $fayinguiz
 	// ];
 	// retrieve the session token
 	$scope.csrf_token = $window.csrf_token;
+	$scope.newwordadded ='';
 	$http.get('/guestaddedword?fayinguizeid=<?php echo $fayinguizeid ?>&yinbiaoid=<?php echo $yinbiaoid ?>').success(function(guestaddedwords)
 	{
 		$scope.wordsadded = guestaddedwords;
@@ -18,12 +19,16 @@ guestaddwordapplication.controller('guestaddwordController_<?php echo $fayinguiz
 			_token: $scope.csrf_token,
 			createdby: 'zhangsan'
 		};
+		var regex = /[a-z]+/;
+		if (regex.test(wordadded.wordtext)){
 		console.log(wordadded);
+		console.log(regex.test(wordadded.wordtext));
 		$http.post('/guestaddedword',wordadded);
 		$scope.wordsadded.push({
 			wordtext: $scope.newwordadded,
 			createdby:'wangwu'
 		});
+		}
 		// $log.info($scope.wordsadded);
 		$scope.newwordadded = '';
 	};
--- trunk/app/views/site/partials/guestaddword.php
+++ trunk/app/views/site/partials/guestaddword.php
@@ -1,7 +1,9 @@
 								
-								    <li class="inlineblock guestaddword" ng-repeat="wordadded in wordsadded"><span class="label label-default label-sm"><small ng-bind="wordadded.wordtext"></small></span></li>
+								    <li class="inlineblock guestaddword" ng-repeat="wordadded in wordsadded"><span class="label label-default label-sm wordaddlabel"><small ng-bind="wordadded.wordtext"></small></span></li>
 									<form style="display:inline" class="form-inline" ng-submit="addword()">
-									    <input type="text" name="wordadded" id="inputWordadded" class="guestaddwordinput" ng-model="newwordadded">
-										<button type="submit" class="btn btn-info btn-xs">我来加词</button>
+									    <span style="display:inline-block">
+									    	<input type="text" name="wordadded" id="inputWordadded" class="guestaddwordinput" ng-model="newwordadded">
+											<button type="submit" class="btn btn-info btn-xs">我来加词</button>
+										</span>
 									</form>					
 							
