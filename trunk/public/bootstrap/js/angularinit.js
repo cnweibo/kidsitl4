d43e0121f9406b668409fc7d618dbddcf70bd845
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2014-08-05 16:18:50 +0000

    当鼠标通过单词时直接animate相应word block的背景颜色

--- trunk/public/bootstrap/js/angularinit.js
+++ trunk/public/bootstrap/js/angularinit.js
@@ -1,7 +1,16 @@
 var guestaddwordapplication = angular.module('guestaddwordapp', []);
 guestaddwordapplication.controller('mp3playerController', function($scope,$window,$log,$http){
-	$scope.playmp3 = function(mymp3){
+	var wordiddombg = "#FFE6FA";
+	var previousanimatingID;
+	$scope.playmp3 = function(mymp3,domid){
 	$("#yinbiaoplayer").attr("src" ,"http://kidsit.cn/getmp3/"+mymp3);
 	$("#yinbiaoplayer").trigger('play');
+
+	//animate the word hovered backgroundColor
+	$(previousanimatingID).stop();
+	$(domid).stop();
+	$(domid).animate({backgroundColor: "#abcdef"}, 800);
+	$(domid).animate({backgroundColor: wordiddombg}, 200);
+	previousanimatingID = domid;
 };
 });
