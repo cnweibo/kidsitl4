fa3aea712a1a0a5f1b33f74647770b6e7d488f70
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2014-09-01 15:56:11 +0000

    增加两位数加法，并且调整试卷答案查看页面style

--- app/views/site/mathexercise/examshow.blade.php
+++ app/views/site/mathexercise/examshow.blade.php
@@ -16,17 +16,21 @@
 @section('content')
 <h1 style="text-align: center;">IT宝贝网低年级数学计算同步练习题库(试卷id:{{$examid}}) <a href="javascript:window.print()"><span class="glyphicon glyphicon-print fa-2x"></span></a></h1>
 <hr>
-	<div class="container">	
+	<div id="examcontainer" class="container">	
+		{{--*/$layoutloopcount=0;/*--}}
 		@foreach (array_chunk($exercises,3) as $row)
 			<div class="row">
 				<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 col-lg-push-1 ">
 					<div class="row	exerciserow">
 						@foreach ($row as $exercise)
+						{{--*/++$layoutloopcount;/*--}}
 							<article class="col-md-4">
-								<span style="display:inline-block;width:45px;">{{$exercise['operand1']}}</span>  
-								<span class="glyphicon glyphicon-plus"></span> 
-								<span style="display:inline-block;width:45px;">{{$exercise['operand2']}}</span>  
-								= (<span style="display:inline-block;width:40px;color: blue;">{{$exercise['sumdata']}}</span>      )
+								<span style="display:inline-block;width:30px;font-size:0.6em" class="label label-success">{{$layoutloopcount}}</span>
+								<span style="display:inline-block;width:40px;font-size: 0.8em">{{$exercise['operand1']}}</span> 
+								<span style="display:inline-block;width:20px;font-size: 0.8em">+</span>  
+								
+								<span style="display:inline-block;width:40px;font-size: 0.8em">{{$exercise['operand2']}}</span>  
+								= <span class="answerdata" data-ng-show="mathexam.showAnswer">({{$exercise['sumdata']}})</span>
 							</article>
 						@endforeach
 					</div>
