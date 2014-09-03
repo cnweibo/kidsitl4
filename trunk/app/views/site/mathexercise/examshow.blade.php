068945d1c8edbe7c4b5ad320b45478a4bb90564f
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2014-09-03 15:38:51 +0000

    小bug examshow.blade.php

--- trunk/app/views/site/mathexercise/examshow.blade.php
+++ trunk/app/views/site/mathexercise/examshow.blade.php
@@ -15,7 +15,7 @@
 @stop
 @section('content')
 <h1 style="text-align: center;">IT宝贝网低年级数学计算同步练习题库(试卷id:{{$examid}}) <a href="javascript:window.print()"><span class="glyphicon glyphicon-print fa-2x"></span></a></h1>
-<h4 style="margin-bottom: 10px;" class="aligncenter">本试卷创建于 {{$examcreatedat}} <span class="label label-warning">试卷永久网址:</span> <a href="http://kidsit.cn/math/exams/[[examdata.examID]]">http://kidsit.cn/math/exams/{{$examid}}</a></h4> 
+<h4 style="margin-bottom: 10px;" class="aligncenter">本试卷创建于 {{$examcreatedat}} <span class="label label-warning">试卷永久网址:</span> <a href="http://kidsit.cn/math/exams/{{$examid}}">http://kidsit.cn/math/exams/{{$examid}}</a></h4> 
 <hr>
 	<div id="examcontainer" class="container">	
 		{{--*/$layoutloopcount=0;/*--}}
