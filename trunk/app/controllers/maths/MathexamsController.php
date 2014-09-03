ad8aeb0fe62f89a4316695ba9fe85ce5107256d5
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2014-09-03 15:14:14 +0000

    创建试卷以及永久静态展示试卷

--- trunk/app/controllers/maths/MathexamsController.php
+++ trunk/app/controllers/maths/MathexamsController.php
@@ -9,35 +9,28 @@ class MathexamsController extends \BaseController {
 	 */
 	public function show($examid)
 	{
+		$mathexam = Mathexam::findOrFail($examid);
 
-		$mathexamExerciseIDs = Mathexam::findOrFail($examid)->exerciseids;
+		$mathexamExerciseIDs = $mathexam->exerciseids;
+		$tablename = $mathexam->exercisetab;
+		$examcreatedat = $mathexam->created_at;
+		switch ($tablename) {
+			case 'mathsum4exercises':
+				$exerciseModel = 'Mathsum4';
+				break;
+			case 'mathsum2exercises':
+				$exerciseModel = 'Mathsum2';
+				break;
+			default:
+				# code...
+				break;
+		}
 		// dd(($mathexamExerciseIDs));
 		$examIDs = json_decode($mathexamExerciseIDs);
 		// 注意使用DB::raw带 FIELD(id, examIDs)将会取消默认的按照value排序的方式
-		$exercises = Mathsum4::whereIn('id', $examIDs)->orderBy(DB::raw("FIELD(id,".implode(",", $examIDs).")"))->get()->toArray();
-		// dd($exercises);
-
-		// $exercises = Mathsum4::whereIn('id',json_decode($mathexamExerciseIDs))->get()->toArray();
-		// dd($exercises);
-		// dd($mathexam);
-		// $difficulty = Input::get('difficulty')? Input::get('difficulty') : 4;
-		// $quantity = Input::get('quantity')? Input::get('quantity'): 100;
-		// $exercises = [];
-		// $exam = [];
-		// $exercises = Mathsum4::orderByRaw("rand() limit 0,{$quantity}")->get()->toArray();
-
-		// // dd($exercises);
-		// //保存卷子到数据库mathexams
-		// for ($i=0;$i<$quantity;$i++){
-		// 	array_push($exam,$exercises[$i]['id']);
-		// }	
-		// $exam_exercisesrows= json_encode($exam);
-		// $mathexam = new Mathexam;
-		// $mathexam-> exerciseids = $exam_exercisesrows;
-		// $mathexam-> exercisetab = "mathsum4exercises";				
-		// $mathexam->save();
-		// $mathexamID = $mathexam -> id ;	
-		return View::make('site.mathexercise.examshow',compact('exercises','examid'));
+		$exercises = $exerciseModel::whereIn('id', $examIDs)->orderBy(DB::raw("FIELD(id,".implode(",", $examIDs).")"))->get()->toArray();
+		
+		return View::make('site.mathexercise.examshow',compact('exercises','examid','examcreatedat'));
 
 
 
