152b79fc5ee6314fb83dce3068b709eba5a04fac
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2014-11-30 17:07:32 +0000

    release version for todoApp

--- trunk/public/Gruntfile.js
+++ trunk/public/Gruntfile.js
@@ -2,48 +2,48 @@ module.exports = function (grunt){
 	grunt.initConfig({
 		pkg: grunt.file.readJSON("package.json"),
 		ngAnnotate: {
-	        options: {
-	            singleQuotes: true,
-	        },
-	        examapp: {
-	            files: [
-	                {
-	                    expand: true,
-	                    src: ['htmlapp/examApp/examApp.js','htmlapp/syscommon/kidsitanimatelib.js'],
-	                    ext: '.annotated.js', // Dest filepaths will have this extension.
-	                    extDot: 'last',       // Extensions in filenames begin after the last dot
-	                },
-	            ],
-	        },
-	        yinbiaoapp: {
-	            files: [
-	                {
-	                    expand: true,
-	                    src: ['htmlapp/yinbiaoApp/angularinit.js','htmlapp/yinbiaoApp/highlightppattern.js',
+			options: {
+				singleQuotes: true,
+			},
+			examapp: {
+				files: [
+					{
+						expand: true,
+						src: ['htmlapp/examApp/examApp.js','htmlapp/syscommon/kidsitanimatelib.js'],
+						ext: '.annotated.js', // Dest filepaths will have this extension.
+						extDot: 'last',       // Extensions in filenames begin after the last dot
+					},
+				],
+			},
+			yinbiaoapp: {
+				files: [
+					{
+						expand: true,
+						src: ['htmlapp/yinbiaoApp/angularinit.js','htmlapp/yinbiaoApp/highlightppattern.js',
 							'htmlapp/syscommon/kidsitanimatelib.js','htmlapp/yinbiaoApp/guestaddword.js','htmlapp/yinbiaoApp/yinbiaoapp.js',],
-	                    ext: '.annotated.js', // Dest filepaths will have this extension.
-	                    extDot: 'last',       // Extensions in filenames begin after the last dot
-	                },
-	            ],
-	        },
-	        todoapp: {
-	            files: [
-	                {
-	                    expand: true,
-			            src:[
-			            		'htmlapp/todoApp/common/base.js',
+						ext: '.annotated.js', // Dest filepaths will have this extension.
+						extDot: 'last',       // Extensions in filenames begin after the last dot
+					},
+				],
+			},
+			todoapp: {
+				files: [
+					{
+						expand: true,
+							src:[
+								'htmlapp/todoApp/common/base.js',
 								'htmlapp/todoApp/app.js',
 								'htmlapp/todoApp/todoCtrl.js',
 								'htmlapp/todoApp/todoStorage.js',
 								'htmlapp/todoApp/todoFocus.js',
 								'htmlapp/todoApp/todoEscape.js'
 							],
-	                    ext: '.annotated.js', // Dest filepaths will have this extension.
-	                    extDot: 'last',       // Extensions in filenames begin after the last dot
-	                },
-	            ],
-	        },
-	    },
+						ext: '.annotated.js', // Dest filepaths will have this extension.
+						extDot: 'last',       // Extensions in filenames begin after the last dot
+					},
+				],
+			},
+		},
 		concat: {
 			options: {
 				seperator: ";",
@@ -51,16 +51,16 @@ module.exports = function (grunt){
 			},
 			math: {
 				src: ["htmlapp/libs/jquery/dist/jquery.min.js","htmlapp/libs/jquery-color/jquery.color.js","htmlapp/libs/bootstrap/dist/bootstrap.min.js","htmlapp/syscommon/custom.js","htmlapp/libs/angular/angular.min.js",
-					  "htmlapp/libs/angular-route/angular-route.min.js","htmlapp/libs/angular-timer.js","htmlapp/libs/angular-animate/angular-animate.min.js","htmlapp/libs/angular-toastr/dist/angular-toastr.js",
-					  "htmlapp/libs/angular-bootstrap/ui-bootstrap-tpls.min.js","htmlapp/libs/TweenMax.min.js","htmlapp/examApp/examApp.annotated.js","htmlapp/syscommon/kidsitanimatelib.annotated.js"],
+						"htmlapp/libs/angular-route/angular-route.min.js","htmlapp/libs/angular-timer.js","htmlapp/libs/angular-animate/angular-animate.min.js","htmlapp/libs/angular-toastr/dist/angular-toastr.js",
+						"htmlapp/libs/angular-bootstrap/ui-bootstrap-tpls.min.js","htmlapp/libs/TweenMax.min.js","htmlapp/examApp/examApp.annotated.js","htmlapp/syscommon/kidsitanimatelib.annotated.js"],
 				dest: "concat/appMath.concat.js"
 			},
-			yinbiao: { 
+			yinbiao: {
 				src: ["htmlapp/libs/jquery/dist/jquery.min.js","htmlapp/libs/jquery-color/jquery.color.js","htmlapp/libs/bootstrap/dist/bootstrap.min.js","htmlapp/syscommon/custom.js","htmlapp/libs/angular/angular.min.js",
-					  "htmlapp/libs/angular-route/angular-route.min.js","htmlapp/yinbiaoApp/angularinit.annotated.js",
-					  "htmlapp/libs/angular-bootstrap/ui-bootstrap-tpls.min.js","htmlapp/libs/TweenMax.min.js",
-					   "htmlapp/yinbiaoApp/highlightppattern.annotated.js","htmlapp/syscommon/kidsitanimatelib.annotated.js","htmlapp/yinbiaoApp/guestaddword.annotated.js",
-					   "htmlapp/libs/angular-animate/angular-animate.min.js","htmlapp/yinbiaoApp/yinbiaoapp.annotated.js"],
+						"htmlapp/libs/angular-route/angular-route.min.js","htmlapp/yinbiaoApp/angularinit.annotated.js",
+						"htmlapp/libs/angular-bootstrap/ui-bootstrap-tpls.min.js","htmlapp/libs/TweenMax.min.js",
+						"htmlapp/yinbiaoApp/highlightppattern.annotated.js","htmlapp/syscommon/kidsitanimatelib.annotated.js","htmlapp/yinbiaoApp/guestaddword.annotated.js",
+						"htmlapp/libs/angular-animate/angular-animate.min.js","htmlapp/yinbiaoApp/yinbiaoapp.annotated.js"],
 				dest: "concat/appYinbiao.concat.js"
 			},
 			todo: {
@@ -81,8 +81,8 @@ module.exports = function (grunt){
 				compress: true,
 				sourceMap: "dist/app.map",
 				mangle: false,
-				banner: "/* copyright <%= pkg.author %> | <%= pkg.license %> " + 
-				        " @<%= grunt.template.today('yyyy-mm-dd') %> */"
+				banner: "/* copyright <%= pkg.author %> | <%= pkg.license %> " +
+						" @<%= grunt.template.today('yyyy-mm-dd') %> */"
 			},
 			math: {
 				src: "concat/appMath.concat.js",
@@ -98,15 +98,15 @@ module.exports = function (grunt){
 			}
 		},
 		targethtml: {
-		  mathrelease: {
-		    files: {
-		      '../app/views/site/mathexercise/examcreate.blade.php': '../app/views/site/mathexercise/examcreate.blade.php.htm'
-		    }
-		  },
-		  mathdev: {
-		    files: {
-		      '../app/views/site/mathexercise/examcreate.blade.php': '../app/views/site/mathexercise/examcreate.blade.php.htm'
-		    }
+			mathrelease: {
+			files: {
+				'../app/views/site/mathexercise/examcreate.blade.php': '../app/views/site/mathexercise/examcreate.blade.php.htm'
+			}
+			},
+		mathdev: {
+			files: {
+				'../app/views/site/mathexercise/examcreate.blade.php': '../app/views/site/mathexercise/examcreate.blade.php.htm'
+			}
 		  },
 		  yinbiaorelease: {
 		    files: {
