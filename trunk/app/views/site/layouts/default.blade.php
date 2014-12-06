9bffba9a4e2024887aec7659a03b5057bd03a098
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2014-12-06 08:40:17 +0000

    revert back to blade:{{}} angular:[[]]

--- trunk/app/views/site/layouts/default.blade.php
+++ trunk/app/views/site/layouts/default.blade.php
@@ -13,8 +13,8 @@
 		@show"
 		/>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="[[asset('bootstrap/css/bootstrap.css,bootstrap-theme.min.css,angular-toastr.css')]]">
-        <link rel="stylesheet" href="[[asset('bootstrap/css/custom.css')]]">
+        <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.css,bootstrap-theme.min.css,angular-toastr.css')}}">
+        <link rel="stylesheet" href="{{asset('bootstrap/css/custom.css')}}">
         @yield('css')
  		<style>
         body {
@@ -30,17 +30,17 @@
 
 		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
-		  <script src="[[asset('htmlapp/libs/html5shiv.js')]]"></script>
-		  <script src="[[asset('htmlapp/libs/respond.min.js')]]"></script>		  
+		  <script src="{{asset('htmlapp/libs/html5shiv.js')}}"></script>
+		  <script src="{{asset('htmlapp/libs/respond.min.js')}}"></script>		  
 		<![endif]-->
 		
 		<!-- Favicons
 		================================================== -->
-		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="[[[ asset('assets/ico/favicon.ico') ]]]">
-		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="[[[ asset('assets/ico/favicon.ico') ]]]">
-		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="[[[ asset('assets/ico/favicon.ico') ]]]">
-		<link rel="apple-touch-icon-precomposed" href="[[[ asset('assets/ico/favicon.ico') ]]]">
-		<link rel="shortcut icon" href="[[[ asset('assets/ico/favicon.ico') ]]]">
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{{ asset('assets/ico/favicon.ico') }}}">
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{{ asset('assets/ico/favicon.ico') }}}">
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{{ asset('assets/ico/favicon.ico') }}}">
+		<link rel="apple-touch-icon-precomposed" href="{{{ asset('assets/ico/favicon.ico') }}}">
+		<link rel="shortcut icon" href="{{{ asset('assets/ico/favicon.ico') }}}">
 	</head>
 	@section('bodyhead')	
 	<body>
@@ -51,7 +51,7 @@
 			<div id="mainnavbar" class="navbar navbar-default navbar-inverse navbar-fixed-top navbar-top-background">
 				<div class="container">
 	                <div class="navbar-header">
-	                	<img style="width:150;height:40px;margin:5px" src="[[[asset('assets/img/Logo_175x60.png')]]]" alt="IT宝贝网" />
+	                	<img style="width:150;height:40px;margin:5px" src="{{{asset('assets/img/Logo_175x60.png')}}}" alt="IT宝贝网" />
 	                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
 	                        <span class="sr-only">切换</span>
 	                        <span class="icon-bar"></span>
@@ -61,13 +61,13 @@
 	                </div>
 	                <div class="collapse navbar-collapse navbar-ex1-collapse">
 	                    <ul class="nav navbar-nav"  id="top-nav-left">
-							<li [[set_active('/')]]><a href="[[[ URL::to('') ]]]">首页</a></li>
-							<li [[set_active('bishun')]]><a href="[[[ URL::to('/bishun') ]]]">笔顺学习</a></li>
-							<li [[set_active('pinyin')]]><a href="[[[ URL::to('/pinyin') ]]]">拼音速学</a></li>
-							<li [[set_active('game')]]><a href="[[[ URL::to('/game') ]]]">键盘练习</a></li>
-							<li [[set_active('yinbiao')]]><a href="[[[ URL::to('yinbiaocategory') ]]]">英语音标</a></li>
-							<li [[set_active('exercise')]]><a href="[[[ URL::to('math/exams') ]]]">中小学同步课堂</a></li>						
-							<li [[set_active('kidsinternet')]]><a href="[[[ URL::to('kidsinternet') ]]]">互联网那点事儿</a></li>						
+							<li {{set_active('/')}}><a href="{{{ URL::to('') }}}">首页</a></li>
+							<li {{set_active('bishun')}}><a href="{{{ URL::to('/bishun') }}}">笔顺学习</a></li>
+							<li {{set_active('pinyin')}}><a href="{{{ URL::to('/pinyin') }}}">拼音速学</a></li>
+							<li {{set_active('game')}}><a href="{{{ URL::to('/game') }}}">键盘练习</a></li>
+							<li {{set_active('yinbiao')}}><a href="{{{ URL::to('yinbiaocategory') }}}">英语音标</a></li>
+							<li {{set_active('exercise')}}><a href="{{{ URL::to('math/exams') }}}">中小学同步课堂</a></li>						
+							<li {{set_active('kidsinternet')}}><a href="{{{ URL::to('kidsinternet') }}}">互联网那点事儿</a></li>						
 							<li class="dropdown hidden">
 								<a class="dropdown-toggle" data-toggle="dropdown" href="#">Dropdown <b class="caret"></b></a>
 								<ul class="dropdown-menu">
@@ -83,13 +83,13 @@
 	                    <ul class="nav navbar-nav pull-right" id="top-nav-right">
 	                        @if (Auth::check())
 	                        @if (Auth::user()->hasRole('admin'))
-	                        <li><a href="[[[ URL::to('admin') ]]]">管理控制台</a></li>
+	                        <li><a href="{{{ URL::to('admin') }}}">管理控制台</a></li>
 	                        @endif
-	                        <li><a href="[[[ URL::to('user') ]]]">登录为： [[[ Auth::user()->username ]]]</a></li>
-	                        <li><a href="[[[ URL::to('user/logout') ]]]">退出</a></li>
+	                        <li><a href="{{{ URL::to('user') }}}">登录为： {{{ Auth::user()->username }}}</a></li>
+	                        <li><a href="{{{ URL::to('user/logout') }}}">退出</a></li>
 	                        @else
-	                        <li [[ (Request::is('user/login') ? ' class="active"' : '') ]]><a href="[[[ URL::to('user/login') ]]]">登录</a></li>
-	                        <li [[ (Request::is('user/register') ? ' class="active"' : '') ]]><a href="[[[ URL::to('user/create') ]]]">[[[ Lang::get('site.sign_up') ]]]</a></li>
+	                        <li {{ (Request::is('user/login') ? ' class="active"' : '') }}><a href="{{{ URL::to('user/login') }}}">登录</a></li>
+	                        <li {{ (Request::is('user/register') ? ' class="active"' : '') }}><a href="{{{ URL::to('user/create') }}}">{{{ Lang::get('site.sign_up') }}}</a></li>
 	                        @endif
 	                    </ul>
 	                @show    
@@ -118,7 +118,7 @@
 
 	    <div id="footer">
 			<div class="container">
-				<p class="muted credit aligncenter inlineblock">版权所有 © [[date('Y')]] <a href="http://kidsit.cn">IT宝贝网</a> <span> | </span> <a target="_blank" href="http://shang.qq.com/wpa/qunwpa?idkey=00124dc114c0de6a4a9d49ad355c597a8abd9fdf82e3a0ea5aed445f1db69cee"><img border="0" src="http://pub.idqqimg.com/wpa/images/group.png" alt="IT宝贝网学员学习群" title="IT宝贝网学员学习群"></a></p>
+				<p class="muted credit aligncenter inlineblock">版权所有 © {{date('Y')}} <a href="http://kidsit.cn">IT宝贝网</a> <span> | </span> <a target="_blank" href="http://shang.qq.com/wpa/qunwpa?idkey=00124dc114c0de6a4a9d49ad355c597a8abd9fdf82e3a0ea5aed445f1db69cee"><img border="0" src="http://pub.idqqimg.com/wpa/images/group.png" alt="IT宝贝网学员学习群" title="IT宝贝网学员学习群"></a></p>
 				<!-- baidu tongji -->
 				<script type="text/javascript">
 				var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
