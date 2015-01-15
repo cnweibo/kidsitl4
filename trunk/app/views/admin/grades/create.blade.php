c886eab0da538fcbad9e85f28c71263d10b48faa
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2015-01-15 17:25:58 +0000

    创建grade ok

---
+++ trunk/app/views/admin/grades/create.blade.php
@@ -0,0 +1,15 @@
+@extends('admin.layouts.modal')
+@section('content')
+	{{ Form::open(['action' => 'AdminGradesController@store']) }}
+		<div class="form-group">
+			{{ Form::label('skillgradetitle', '年级') }}
+			{{ Form::text('skillgradetitle', null, ['class' => 'form-control']) }}
+		</div>
+		<div class="form-group">
+			{{ Form::label('skillgradedescription', '详细描述') }}
+			{{ Form::text('skillgradedescription', null, ['class' => 'form-control']) }}
+		</div>
+		{{ Form::submit('提交', ['class' => 'btn btn-primary']) }}
+	{{ Form::close() }}
+
+@stop
---
+++ trunk/app/views/admin/grades/delete.blade.php
@@ -0,0 +1,1 @@
+app/views/admin/grades/delete.blade.php
