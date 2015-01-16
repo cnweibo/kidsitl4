1e9821d4c6518f7d40e2712bb82431f064137369
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2015-01-16 18:11:44 +0000

    grade后台edit正常

--- app/views/admin/grades/edit.blade.php
+++ app/views/admin/grades/edit.blade.php
@@ -1,1 +1,14 @@
-app/views/admin/grades/edit.blade.php
+@extends('admin.layouts.modal')
+@section('content')
+	{{ Form::open(['action' => array('AdminGradesController@update', $grade->id),'method' => 'PUT']) }}
+		<div class="form-group">
+			{{ Form::label('skillgradetitle', '年级') }}
+			{{ Form::text('skillgradetitle', $grade->skillgradetitle, ['class' => 'form-control','autofocus']) }}
+		</div>
+		<div class="form-group">
+			{{ Form::label('skillgradedescription', '详细描述') }}
+			{{ Form::text('skillgradedescription', $grade->skillgradedescription, ['class' => 'form-control']) }}
+		</div>
+		{{ Form::submit('提交', ['class' => 'btn btn-primary']) }}
+	{{ Form::close() }}
+@stop
