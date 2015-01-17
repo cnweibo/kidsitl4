e3d985c4f9ac08f6e06c35aa01b8cda670704be7
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2015-01-17 07:24:53 +0000

    feature:增加grade edit页面中的定制化pagination功能

--- app/views/admin/grades/edit.blade.php
+++ app/views/admin/grades/edit.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 	{{ Form::open(['action' => array('AdminGradesController@update', $grade->id),'method' => 'PUT']) }}
 		<div class="form-group">
-			{{ Form::label('skillgradetitle', '年级') }}
+			{{ Form::label('skillgradetitle', '年级') }} <a href="{{action('AdminGradesController@index')}}"> <span class="badge">{{$totoalids}}</span></a>
 			{{ Form::text('skillgradetitle', $grade->skillgradetitle, ['class' => 'form-control','autofocus']) }}
 		</div>
 		<div class="form-group">
@@ -10,5 +10,12 @@
 			{{ Form::text('skillgradedescription', $grade->skillgradedescription, ['class' => 'form-control']) }}
 		</div>
 		{{ Form::submit('提交', ['class' => 'btn btn-primary']) }}
+		<nav>
+		  <ul class="pager">
+		    <li><a href="{{action('AdminGradesController@edit', $previousid)}}">Previous</a></li>
+		    <li><a href="{{action('AdminGradesController@edit', $nextid)}}">Next</a></li>
+		  </ul>
+		</nav>
+	<hr>
 	{{ Form::close() }}
 @stop
