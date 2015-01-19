97ecda5fee383b72b3ddbe4c27103e6ba698cce3
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2015-01-19 06:25:46 +0000

    feature: edit in place for grade page

--- public/htmlapp/system/grade/gradeApp.mod.js
+++ public/htmlapp/system/grade/gradeApp.mod.js
@@ -3,7 +3,7 @@
     'use strict';
     document.write("<base href='http://kidsit.cn/htmlapp/system/grade/' />");
    
-    angular.module('gradeApp',['ngRoute','khttp','cgBusy'])
+    angular.module('gradeApp',['ngRoute','khttp','cgBusy','xeditable'])
     .config(['$routeProvider','$interpolateProvider', function ($routeProvider,$interpolateProvider) {
         $interpolateProvider.startSymbol('[[');
         $interpolateProvider.endSymbol(']]');
@@ -14,5 +14,8 @@
 		$routeProvider.when('/grade-detail/:id', {templateUrl: 'system/grade/partials/show.html', controller: 'gradeDetailCtrl'});
 		$routeProvider.otherwise({redirectTo: '/grade-list'});
 
-		}]);
+		}])
+    .run(function(editableOptions) {
+        editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+    });
 })();
