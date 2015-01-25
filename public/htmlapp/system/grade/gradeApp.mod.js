992740c690e2f578905387abcc10692c0c56036a
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2015-01-25 11:39:34 +0000

    grade management web app ok now!

--- public/htmlapp/system/grade/gradeApp.mod.js
+++ public/htmlapp/system/grade/gradeApp.mod.js
@@ -5,9 +5,33 @@
     var assetbase = "http://kidsit.cn/htmlapp/system/grade/";
     var indexpagebase = "http://kidsit.cn/admin/system/grade#";
     angular.module('gradeApp',['ngRoute','khttp','cgBusy','xeditable','toastr','ui.utils'])
-    .config(['$routeProvider','$interpolateProvider', function ($routeProvider,$interpolateProvider) {
+    .config(['$routeProvider','$interpolateProvider', 'toastrConfig',function ($routeProvider,$interpolateProvider,toastrConfig) {
         $interpolateProvider.startSymbol('[[');
         $interpolateProvider.endSymbol(']]');
+
+        angular.extend(toastrConfig, {
+            allowHtml: true,
+            closeButton: true,
+            closeHtml: '<button>&times;</button>',
+            containerId: 'toast-container',
+            extendedTimeOut: 1000,
+            iconClasses: {
+              error: 'toast-error',
+              info: 'toast-info',
+              success: 'toast-success',
+              warning: 'toast-warning'
+            },
+            maxOpened: 0,
+            messageClass: 'toast-message',
+            newestOnTop: true,
+            onHidden: null,
+            onShown: null,
+            positionClass: 'toast-bottom-full-width',
+            tapToDismiss: true,
+            timeOut: 5000,
+            titleClass: 'toast-title',
+            toastClass: 'toast'
+          });
 
         
 		$routeProvider.when('/create', {templateUrl: 'http://kidsit.cn/htmlapp/system/grade/partials/create.html', controller: 'gradeCreateCtrl'});
