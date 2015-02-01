63dff2fa4a91ae77358982c7ffb5286f865298d1
kidsitadmin <kidsitadmin@ef504420-0a33-4974-859f-2d8f832d02a9>
2015-02-01 16:08:44 +0000

    classroom创建时，当form表单invalid时，拒绝提交表单，并且提示

--- public/htmlapp/system/classroom/classroomCreate.ctrl.js
+++ public/htmlapp/system/classroom/classroomCreate.ctrl.js
@@ -28,7 +28,11 @@
         );
         // $scope.teachersdata = [{email:"p\u4efd",password:"fsadfsda",cell:"fsd\u4efd",id:3,name:"\u9648\u79c0",sysloginname:"chenxiu",organization:"fs",address:"fs",created_at:"2015-01-25 23:37:58",classes:[{id:11,sysname:"gffdsfda",description:"a \u5728\u91cd\u8bfb\u5f00\u97f3\u8282\u4e2d\uff0c\u53d1\u5b57\u6bcd\u97f3[ei]",profileURL:"fssafdfs",deleted_at:null,created_at:"2015-01-31 22:34:00",updated_at:"2015-01-31 22:34:00",teacher_id:3}]},{email:"fff\u53d1\u653e\u901f\u5ea6\u901f\u5ea6",password:"fsdfdsafsd",cell:"fff",id:25,name:"alice",sysloginname:"alicezhang",organization:"fff",address:"fff",created_at:"2015-01-26 01:09:30",classes:[]},{email:"dsdsdsd",password:"fdsfsda",cell:"dddd",id:26,name:"ffffsdsd",sysloginname:"dsddsds",organization:"dddd",address:"desdsddss",created_at:"2015-01-28 00:21:34",classes:[]}];
         $scope.selected = null;
-        vm.createClassroom = function () {
+        vm.createClassroom = function (form) {
+			if (form.$invalid){
+				toastr.error('请先改正表单中的错误字段后重试！');
+				return;
+			}
 			var promise;
 			vm.newClassroom._token = $window._token;
 			vm.currentPromise = promise = khttp.store("http://kidsit.cn/admin/api/system/classroom",vm.newClassroom);
@@ -60,6 +64,9 @@
 				}
 			);
 			return d.promise;
+        };
+        vm.viewFormState = function (form) {
+			console.log(form.$valid);
         };
     }
 })();
